f47f8ad6173b94814f726c46c110b998ba54160a
Alan Broun <abroun@alanbroun.net>
2010-07-30 19:44:45 +0100

    Added a library for utility routines and had a go at adding in a composite widget (which turned out to be unnecessary)

---
+++ abroun_libs/abroun_gtk_gui/src/abroun_gtk_gui/widgets/FilePath.py
@@ -0,0 +1,79 @@
+#-------------------------------------------------------------------------------
+import os.path
+import pygtk
+pygtk.require('2.0')
+import gtk
+
+#-------------------------------------------------------------------------------
+class FilePath( gtk.VBox ):
+    """
+    Composite widget consisting of a textbox and a button which allows a user to
+    either enter a filename or else click the button to open a file selector
+    dialog
+    """
+
+    __gtype_name__ = 'FilePath'
+
+    #---------------------------------------------------------------------------
+    def __init__( self ):
+        gtk.VBox.__init__( self )
+
+        self.filter = None
+        self.startingFolder = None
+
+        gtk.widget_push_composite_child()
+        
+        builder = gtk.Builder()
+        scriptPath = os.path.dirname( __file__ )
+        builder.add_from_file( scriptPath + "/glade/FilePath.glade" )
+        
+        window = builder.get_object( "window1" )
+        topBox = builder.get_object( "topBox" )
+        self.tbxFile = builder.get_object( "tbxFile" )
+        
+        window.remove( topBox )
+        self.pack_start( topBox )
+        builder.connect_signals( self )
+
+        gtk.widget_pop_composite_child()
+        
+    #---------------------------------------------------------------------------
+    def setFilter( self, filter ):
+        
+        self.filter = filter
+    
+    #---------------------------------------------------------------------------
+    def setStartingFolder( self, startingFolder ):
+        
+        self.startingFolder = startingFolder
+        
+    #---------------------------------------------------------------------------
+    def getFilename( self ):
+        return self.tbxFile.get_text()
+       
+    #---------------------------------------------------------------------------
+    def onBtnFileClicked( self, widget, data = None ):
+    
+        dialog = gtk.FileChooserDialog(
+            title="Choose Output File",
+            action=gtk.FILE_CHOOSER_ACTION_SAVE,
+            buttons=(gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT,
+                      gtk.STOCK_OK, gtk.RESPONSE_ACCEPT) )
+
+        if self.startingFolder != None:
+            dialog.set_current_folder( self.startingFolder )
+            
+        if self.filter != None:
+            dialog.add_filter( self.filter )
+            dialog.set_filter( self.filter )
+            
+        result = dialog.run()
+
+        if result == gtk.RESPONSE_ACCEPT:
+            filename = dialog.get_filename()
+            #if os.path.splitext( filename )[ 1 ] == "":
+            #    filename += ".yaml"
+
+            self.tbxFile.set_text( filename )
+
+        dialog.destroy()
